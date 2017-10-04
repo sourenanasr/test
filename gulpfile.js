94348600817d9b904ec8d50a1ab5c42c46682259
Nima Baghdadi <bg.nima@gmail.com>
2017-10-05 00:19:27 +0330

    replaced brunch with gulp, used template engine for static pages + small file structure change

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp            = require("gulp");
+var babelify        = require("babelify");
+var browserify      = require("browserify");
+var source          = require("vinyl-source-stream");
+var sass            = require('gulp-sass');
+var autoprefixer    = require('gulp-autoprefixer');
+var flatten         = require('gulp-flatten');
+var rename          = require('gulp-rename');
+var swig            = require('gulp-swig');
+
+const paths = {
+  dist: './public'
+};
+
+gulp.task("script", function(){
+  return browserify({entries: ["./app/assets/js/initialize.js"]})
+      .transform(babelify)
+      .bundle()
+      .pipe(source("app.js"))
+      .pipe(gulp.dest(paths.dist+"/assets/js"));
+});
+
+gulp.task("statics", function(){
+  return gulp.src("./app/assets/images/**/*.*")
+      .pipe(gulp.dest(paths.dist+"/assets/images/"));
+});
+
+gulp.task("stylesheet", function() {
+  return gulp.src('./app/assets/stylesheets/**/*.s*ss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(flatten())
+      .pipe(autoprefixer(['last 2 versions', 'ie 8', 'ie 9']))
+      .pipe(rename("app.css"))
+      .pipe(gulp.dest(paths.dist+"/assets/css"))
+});
+
+gulp.task("swig", function(){
+  return gulp.src(["./app/**/*.html","!./app/**/_*.html"])
+      .pipe(swig())
+      .pipe(gulp.dest(paths.dist));
+});
+
+
+gulp.task("build", ["script", "statics", "stylesheet", "swig"]);
